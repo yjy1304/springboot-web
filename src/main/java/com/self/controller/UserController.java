de36d773d3cf26f7f82bd445763e07436dc59fbd
weilong <weilong@wacai.com>
2016-01-30 09:09:41 +0800

    增加Springloaded热部署插件

--- src/main/java/com/self/controller/UserController.java
+++ src/main/java/com/self/controller/UserController.java
@@ -1,23 +1,27 @@
 package com.self.controller;
 
 import com.self.entity.User;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by wacai on 2016/1/7.
  */
-@RestController
-@RequestMapping("/user")
+@Controller
+@RequestMapping(value="/user")
 public class UserController {
-
-    @RequestMapping("/{id}")
-    public User view(@PathVariable("id") Long id) {
-        User user = new User();
-        user.setId(id);
-        user.setName("zhang");
+    Logger logger = LoggerFactory.getLogger(UserController.class);
+    @RequestMapping(value="/create", method = {RequestMethod.POST})
+    @ResponseBody
+    public User view(User user) {
+        logger.info("id:" + user.getId() + ",name=" + user.getName());
         return user;
+    }
+    @RequestMapping(value="/initUser")
+    public String init(){
+        return "/createUser";
     }
 
 }
--- src/main/java/com/self/entity/User.java
+++ src/main/java/com/self/entity/User.java
@@ -39,4 +39,12 @@ public class User {
     public int hashCode() {
         return id != null ? id.hashCode() : 0;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
